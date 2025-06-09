08c725293ebe1c201f48990311e2ce6175565847
qlansb <qlansbury14@gmail.com>
2025-06-09 16:10:09 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './app';
 import { AuthProvider } from './Auth/AuthContext';
 import { BrowserRouter } from 'react-router-dom';
 
